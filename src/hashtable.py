a5b5dda36386fdde8e32db678d65dfe729b3533a
Shannon <shannon.yoshikawa@gmail.com>
2020-01-16 09:51:27 -0800

    with the files this time

--- src/hashtable.py
+++ src/hashtable.py
@@ -16,7 +16,6 @@ class HashTable:
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
 
-
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
@@ -51,6 +50,26 @@ class HashTable:
 
         Fill this in.
         '''
+        #TODO: finish this
+        index = self._hash_mod(key)
+        if self.storage[index]:
+            linked_list=self.storage[index]
+            while linked_list:
+                if linked_list.key == key:
+                    #print(f"original: {linked_list.key}, {linked_list.value}")
+
+                    linked_list.value = value
+                    #print(f"new: {key}, {linked_list.value}")
+                    return
+                else:
+                    if linked_list.next is None:
+                        linked_list.next = LinkedPair(key, value)
+                    else:
+                        linked_list = linked_list.next
+            
+
+        else:
+            self.storage[index] = LinkedPair(key, value)
         pass
 
 
@@ -63,7 +82,32 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        #print("have index")
+        if self.storage[index]:
+            linked_list = self.storage[index]
+            prev = None
+            while linked_list:
+                #print("while")
+                if linked_list.key == key:
+                    #print("linked_list.key == key")
+                    if prev:
+                        #print("prev true")
+                        prev.next = linked_list.next
+                        del linked_list
+                        return
+                    else:
+                        #print("prev false")
+                        self.storage[index] = linked_list.next
+                        del linked_list
+                        return
+                prev = linked_list
+                linked_list = linked_list.next
+                
+        else:
+            print("Key Not Found")
+            return
+
 
 
     def retrieve(self, key):
@@ -74,7 +118,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        index=self._hash_mod(key)
+        if self.storage[index]:
+            linked_item = self.storage[index]
+            while linked_item is not None:
+                if linked_item.key == key:
+                    #print(f"key: {key}, value: {linked_item.value}")
+                    return linked_item.value
+                else:
+                    linked_item = linked_item.next
+            return None
 
 
     def resize(self):
@@ -84,7 +138,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        data = self.storage
+        self.capacity = self.capacity*2
+        self.storage = [None] * self.capacity
+        for linked_list in data:
+            while linked_list is not None:
+                self.insert(linked_list.key, linked_list.value)
+                linked_list = linked_list.next
+        return
+
+        
 
 
 
